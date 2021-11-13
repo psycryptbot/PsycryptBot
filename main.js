a209ecbc195761c775f711f2bb82ef9307c0d1f1
Corban Amouzou <c0dineDev@gmail.com>
2021-11-12 17:53:12 -0800

    Looks like git hates me
    - Improvements
    - Staging
    - maybe a tiny bit of code (still setting up)
    
    Lets get cracking!

---
+++ main.js
@@ -0,0 +1,9 @@
+//
+// PsycryptBot
+// Copyright (c) 2021. Corban Amouzou, Hunter Ummels.
+//
+
+// require('minimist');
+const path = require('path'),
+      version = require('package.json').versi
+      

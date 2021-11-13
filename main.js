d12826bed03a3cb89cca890aa9c5b89d3ed94229
Corban Amouzou <c0dineDev@gmail.com>
2021-11-12 21:11:02 -0800

    Lots of code, improves logger. Startup system.

--- main.js
+++
@@ -1,9 +0,0 @@
-//
-// PsycryptBot
-// Copyright (c) 2021. Corban Amouzou, Hunter Ummels.
-//
-
-// require('minimist');
-const path = require('path'),
-      version = require('package.json').versi
-      
---
+++ src/public/Psycrypt.js
@@ -0,0 +1,52 @@
+//
+// PsycryptBot
+// Copyright (c) 2021. Corban Amouzou, Hunter Ummels.
+//
+
+const minimalist = require('minimist');
+const version = require('../../package.json').version;
+const fs = require('fs');
+// const klaw = require('klaw');
+const EventEmitter = require('events');
+const Logger = require('../logger');
+const path = require('path');
+
+/**
+ * Main state of the entire bot
+ * @class Psycrypt
+ */
+class Psycrypt extends Logger {
+  /**
+   * Creates an instance of Psycrypt.
+   * @param {Array} arguments
+   * @memberof Psycrypt
+   */
+  constructor() {
+    super('Psycrypt');
+    this.overwrites = {};
+    this.config = {};
+    this.version = version;
+    this.arguments = minimalist(process.argv.slice(3));
+    this.events = new EventEmitter();
+
+    // Check for config existance before anything
+    this.ensureConfig();
+  }
+
+  /**
+   * Makes sure that the config exists and calls a setup if not
+   * @memberof Psycrypt
+   */
+  ensureConfig() {
+    const configDir = path.join(__dirname, '../../config.js');
+    const setupDir = path.join(__dirname, '../../startup/scripts/setup');
+    if (fs.existsSync(configDir) && this.config == {}) {
+      this.config = require(configDir);
+    } else {
+      const setup = require(setupDir);
+      setup();
+    }
+  }
+}
+
+module.exports = Psycrypt;

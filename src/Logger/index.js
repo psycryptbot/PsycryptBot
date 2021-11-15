ff18a8618720e7644671518500d93c2c442d6ebb
cstanze <developer.julius0301@gmail.com>
2021-11-15 13:18:44 -0500

    lol its 2021 not 2020

--- src/Logger/index.js
+++ src/Logger/index.js
@@ -2,6 +2,7 @@
 // PsycryptBot
 // -------------------------------------
 // | Copyright © 2021. Corban Amouzou  |
+// | Copyright © 2021. Jules Amalie    |
 // | Copyright © 2021. Hunter Ummels   |
 // -------------------------------------
 // All rights reserved.

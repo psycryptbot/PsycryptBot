6035b9fd47b4e20dfd9f053456a8311fc09b6682
Corban Amouzou <c0dineDev@gmail.com>
2021-11-17 17:13:48 -0800

    Idk, I'll finish config when it is needed

--- dev/setup.js
+++ dev/setup.js
@@ -11,7 +11,12 @@
 
 // eslint-disable-next-line no-unused-vars
 const Psycrypt = require('../src/Psycrypt');
-const {Select} = require('enquirer');
+const {Select, Form} = require('enquirer');
+const fs = require('fs');
+const path = require('path');
+const rootPath = path.join(__dirname, '../');
+const configBase = require(path.join(rootPath, 'config-base'));
+
 // TODO: Guided .env setup
 // TODO: Network .env setup
 
@@ -33,6 +38,22 @@ module.exports = async (psycrypt) => {
     // eslint-disable-next-line max-len
     logger.error(`Sorry, but network is not supported yet ( Sorry to psyc' you out XD )`);
   } else {
-    logger.error(`Please implement me!`);
+    // Am I being lazy? I'm just going to finish this when I actually know
+    // What I am configuring
+
+    // const configQuestion = new Form({
+    //   name: 'config',
+    //   message: 'Please setup your config.js here:',
+    //   choices: Object.keys(configBase).map((key, __unused) => {
+    //     const value = configBase[key];
+    //     return {
+    //       name: key,
+    //       message: value,
+    //     };
+    //   }),
+    // });
+    // const result = await configQuestion.run();
+    // const merged = Object.assign(configBase)
+    // fs.writeFileSync();
   }
 };
